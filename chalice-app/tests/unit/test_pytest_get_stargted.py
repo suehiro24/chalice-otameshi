ea61d2b788f6b1153d2213cbb6363f5d0b21019e
Suehiro24 <hayatasato.0@gmail.com>
2023-10-10 17:30:14 +0000

    feat: add sample testcases with pytest

---
+++ chalice-app/tests/unit/test_pytest_get_stargted.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def plus_one(x):
+    return x + 1
+
+
+@pytest.mark.lerning
+def test_four_plus_one_is_5():
+    assert plus_one(4) == 5
+
+
+@pytest.mark.lerning
+def test_three_plus_one_isnt_5():
+    assert plus_one(3) != 5
+
+
+@pytest.mark.lerning
+def test_three_plus_one_is_5_will_raises_assertion_error():
+    with pytest.raises(AssertionError):
+        assert plus_one(3) == 5
