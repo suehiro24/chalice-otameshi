c350c60a532695b8ebcdd4c1096c3f1f9a8f71aa
Suehiro24 <hayatasato.0@gmail.com>
2023-10-12 14:23:56 +0000

    test: add test case with stubbed external API response

--- chalice-app/tests/unit/otameshi/test_api.py
+++ chalice-app/tests/unit/otameshi/test_api.py
@@ -1,4 +1,4 @@
-# from unittest.mock import patch
+from unittest import mock
 
 from chalice.test import Client
 from chalicelib.app import app
@@ -12,3 +12,18 @@ def test_get_index():
     with Client(app) as client:
         response = client.http.get(f"{url_prefix_otameshi}/")
         assert response.json_body == {"hello": "world"}
+
+
+@mock.patch("chalicelib.otameshi.app_otameshi.requests.get")
+def test_get_posts(mock_get: mock.MagicMock):
+    """Mocking with the patch decorator to get posts from an JsonPlaceholder API"""
+    # Mocking the response from the JsonPlaceholder API
+    mock_get.return_value.ok = True
+    mock_get.return_value.json.return_value = [
+        {"id": 1, "title": "foo"},
+        {"id": 2, "title": "bar"},
+    ]
+    # Testing the route
+    with Client(app) as client:
+        response = client.http.get(f"{url_prefix_otameshi}/jsonplaceholder/posts")
+        assert response.status_code == 200
